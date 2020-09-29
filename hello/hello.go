3630fc8e7cf76ccef31de0c9c86cb915c47d0a2c
Meera Sundar <mirage.sundial@gmail.com>
2020-09-29 23:26:36 +0530

    refactor

--- hello/hello.go
+++ hello/hello.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 const englishGreetingPrefix = "Hello, "
+const spanishGreetingPrefix = "Hola, "
+const spanish = "Spanish"
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "Spanish"{
-		return "Hola, " + name
+	if language == spanish{
+		return spanishGreetingPrefix + name
 	}
 	return englishGreetingPrefix + name
 }
